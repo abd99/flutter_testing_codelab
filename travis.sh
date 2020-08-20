0903187e50d96d8df16c58b99eedb24bceb5545b
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2020-08-20 10:35:38 +0530

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -10,17 +10,13 @@ echo "== FLUTTER: $FLUTTER"
 FLUTTER_VERS=`$FLUTTER --version | head -1`
 echo "== FLUTTER_VERS: $FLUTTER_VERS"
 
-# PluginCodelab is a special case since it's a plugin.  Analysis doesn't seem to be working.
-#pushd $PWD
-#echo "== TESTING PluginCodelab"
-#cd ./PluginCodelab
-#$FLUTTER format --dry-run --set-exit-if-changed .;
-#popd
-
 declare -a PROJECT_PATHS=($(find . -not -path './flutter/*' -not -path './PluginCodelab/pubspec.yaml' -name pubspec.yaml -exec dirname {} \;))
 
 for PROJECT in "${PROJECT_PATHS[@]}"; do
   echo "== TESTING $PROJECT"
+  if ![[ "$PROJECT" == *"testing_codelab"* ]]; then
+    $FLUTTER create --no-overwrite "$PROJECT"
+  fi
   (
     cd "$PROJECT";
     set -x;
